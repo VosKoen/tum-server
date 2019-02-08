679b00a01713d80e7b927f33906de77b8bcf1284
VosKoen <Koen82@gmail.com>
2019-02-08 11:52:24 +0100

    more

--- src/recipe-images/controller.ts
+++ src/recipe-images/controller.ts
@@ -4,24 +4,23 @@ import {
   Param,
   UploadedFile,
   Post,
-  Body
+  Body,
+  HttpCode,
+  NotFoundError
 } from "routing-controllers";
 import { getRepository } from "typeorm";
 import RecipeImage from "./entity";
-import * as ImageKit from 'imagekit'
-import {imageKitId, publicApiKey, privateApiKey} from '../constants'
-
+import Recipe from '../recipes/entity'
+import * as ImageKit from "imagekit";
+import { imageKitId, publicApiKey, privateApiKey } from "../constants";
 
 @JsonController()
 export default class RecipeImageController {
   imagekit = new ImageKit({
-    "imagekitId" : imageKitId,       
-    "apiKey" : publicApiKey,       
-    "apiSecret" : privateApiKey, 
+    imagekitId: imageKitId,
+    apiKey: publicApiKey,
+    apiSecret: privateApiKey
   });
-
-
-
 
   @Get("/recipes/:id/images/random")
   async getRandomImage(@Param("id") id: number) {
@@ -42,29 +41,44 @@ export default class RecipeImageController {
   }
 
   @Post("/images/upload")
+  @HttpCode(201)
   async uploadNewImage(
-    @UploadedFile("file", 
-    ) file: any, @Body() body: any
-  ) 
-  {
-    
-    
+    @UploadedFile("file")
+    file: any,
+    @Body() body: any
+  ) {
     try {
-      const imageBase64 = file.buffer.toString('base64')
-      await this.imagekit.upload(imageBase64, {
-        "filename" : "some-test",
-        "folder" : "another-test"
-      })
+      const imageBase64 = file.buffer.toString("base64");
 
-      console.log(body)
-      
+      let image;
+
+      await this.imagekit
+        .upload(imageBase64, {
+          filename: "some-test",
+          folder: `user/${body.userId}`
+        })
+        .then(result => (image = result));
+
+      return { imageUrl: image.url };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  @Post("/recipes/:id/images/")
+  @HttpCode(201)
+  async addImageToRecipe(@Param("id") id: number, @Body() image: Partial<RecipeImage>) {
+    try {
+      const recipe = await Recipe.findOne(id);
+
+      if (!recipe)
+      throw new NotFoundError("Could not find a recipe with this id");
+      const recipeImage = {recipeId: recipe.id, imageUrl: image.imageUrl}
+      return RecipeImage.create(recipeImage).save()
 
     } catch (error) {
       console.log(error);
     }
-    return {
-      imageUrl:
-        "https://ik.imagekit.io/foxAppDevImages/spaghetti-carbonara_rkLaQdhf4.jpg"
-    };
   }
+
 }
