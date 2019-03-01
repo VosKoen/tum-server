8c3854e71101a5ea85dadc5d26649e0bab69731c
VosKoen <Koen82@gmail.com>
2019-03-01 11:47:01 +0100

    recipe history now available for user, no pagination

--- src/selected-recipes/controller.ts
+++ src/selected-recipes/controller.ts
@@ -1,11 +1,26 @@
-import { JsonController, Param, Post, HttpCode } from "routing-controllers";
+import {
+  JsonController,
+  Param,
+  Post,
+  HttpCode,
+  Get
+} from "routing-controllers";
 import User from "../users/entity";
 import Recipe from "../recipes/entity";
 import SelectedRecipe from "../selected-recipes/entity";
+import { getRepository } from "typeorm";
+
+interface RecipeHistoryEntry {
+  recipeId: SelectedRecipe["recipeId"];
+  title: Recipe["title"];
+  timeSelected: SelectedRecipe["selectedTimestamp"];
+}
+
+interface RecipeHistory extends Array<RecipeHistoryEntry> {}
 
 @JsonController()
 export default class SelectedRecipeController {
-  @Post("/users/:userId/recipes/:recipeId")
+  @Post("/users/:userId/recipes/:recipeId/selected-recipes")
   @HttpCode(201)
   async createRecipeSelected(
     @Param("userId") userId: number,
@@ -21,5 +36,34 @@ export default class SelectedRecipeController {
       user,
       selectedTimestamp
     }).save();
+  }
+
+  @Get("/users/:userId/selected-recipes")
+  async getRecipeHistory(@Param("userId") userId: number) {
+    try {
+      const history = await getRepository(SelectedRecipe)
+        .createQueryBuilder("selectedRecipe")
+        .innerJoinAndSelect("selectedRecipe.recipe", "recipe")
+        .where("selectedRecipe.userId = :userId", { userId })
+        .getMany();
+
+      if (history) {
+        const recipeHistory: RecipeHistory = history.map(item => {
+          const historyItem: RecipeHistoryEntry = {
+            recipeId: item.recipeId,
+            title: item.recipe.title,
+            timeSelected: item.selectedTimestamp
+          };
+          return historyItem;
+        });
+        return recipeHistory;
+      }
+      return [];
+    } catch (error) {
+      console.log(error);
+      throw new Error(
+        "Something went wrong retrieving your cooked recipe history"
+      );
+    }
   }
 }
