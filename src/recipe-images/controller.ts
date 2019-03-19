3051c9e99f1990b3841fa90cf8877d3fa07b5b7d
VosKoen <Koen82@gmail.com>
2019-03-19 15:03:49 +0100

    photos are uploaded upon submitting the recipe

--- src/recipe-images/controller.ts
+++ src/recipe-images/controller.ts
@@ -4,7 +4,6 @@ import {
   Param,
   UploadedFile,
   Post,
-  Body,
   HttpCode,
   NotFoundError
 } from "routing-controllers";
@@ -13,7 +12,7 @@ import { getRepository } from "typeorm";
 import RecipeImage from "./entity";
 import Recipe from "../recipes/entity";
 import * as cloudinary from "cloudinary";
-import {imageFolder, imageTransformGradient} from "../constants"
+import { imageFolder, imageTransformGradient } from "../constants";
 
 @JsonController()
 export default class RecipeImageController {
@@ -28,11 +27,12 @@ export default class RecipeImageController {
           .limit(1)
           .getOne();
 
-        if(!image) throw new NotFoundError("No image found")
+        if (!image) throw new NotFoundError("No image found");
+
 
         //Add tranform Cloudinary
-        const regExp = new RegExp('/v[0-9]+/');
-        image.imageUrl = image.imageUrl.replace(regExp,imageTransformGradient)
+        const regExp = new RegExp("/v[0-9]+/");
+        image.imageUrl = image.imageUrl.replace(regExp, imageTransformGradient);
 
         return image;
       } catch (error) {
@@ -41,48 +41,44 @@ export default class RecipeImageController {
     }
   }
 
-  @Post("/images/upload")
+  @Post("/recipes/:id/images/")
   @HttpCode(201)
-  async uploadNewImage(
-    @UploadedFile("file")
-    file: any
+  async addImageToRecipe(
+    @Param("id") id: number,
+    @UploadedFile("file") file: any
   ) {
-
-    let returnData;
-
+    let cloudinaryReturn;
     try {
       const promise = new Promise((resolve, reject) =>
         cloudinary.v2.uploader
-          .upload_stream({ resource_type: "image", folder: imageFolder }, (error, result) => {
-            if (error) reject(error);
-            resolve(result);
-          })
+          .upload_stream(
+            { resource_type: "image", folder: imageFolder },
+            (error, result) => {
+              if (error) reject(error);
+              resolve(result);
+            }
+          )
           .end(file.buffer)
       );
 
       await promise
-        .then(res => (returnData = res))
+        .then(res => (cloudinaryReturn = res))
         .catch(err => console.log(err));
     } catch (error) {
       console.log(error);
       return "An error occurred";
     }
+    console.log(cloudinaryReturn.secure_url);
 
-    return { imageUrl: returnData.secure_url };
-  }
-
-  @Post("/recipes/:id/images/")
-  @HttpCode(201)
-  async addImageToRecipe(
-    @Param("id") id: number,
-    @Body() image: Partial<RecipeImage>
-  ) {
     try {
       const recipe = await Recipe.findOne(id);
 
       if (!recipe)
         throw new NotFoundError("Could not find a recipe with this id");
-      const recipeImage = { recipeId: recipe.id, imageUrl: image.imageUrl };
+      const recipeImage = {
+        recipeId: recipe.id,
+        imageUrl: cloudinaryReturn.secure_url
+      };
       return RecipeImage.create(recipeImage).save();
     } catch (error) {
       console.log(error);
